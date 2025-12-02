d2f4c126428076a7e7a030e9fede2dc5f78adc8b
Ulriche-Dev <ulriche.m@h2altitude.com>
2025-12-02 10:50:33 +0100

    Feat: Finalisation des opérations CRUD, ajout du style et test

--- taches/tests.py
+++ taches/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Tache
 
-# Create your tests here.
+class TacheTests(TestCase):
+
+    def test_liste_taches_status_code(self):
+        """Vérifie que la page listant les tâches retourne un code 200."""
+        url = reverse('liste_taches')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_creation_tache(self):
+        """Vérifie que la création d'une tâche via POST fonctionne et redirige."""
+        url = reverse('ajouter_tache')
+        data = {
+            'titre': 'Nouvelle Tâche',
+            'description': 'Description de la tâche',
+            'termine': False
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)  # Redirection après création
+        self.assertTrue(Tache.objects.filter(titre='Nouvelle Tâche').exists())
